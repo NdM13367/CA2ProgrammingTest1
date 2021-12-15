e22acbd15dcffe501b7fe0867da60a6983f25a30
niall <D00241147@student.dkit.ie>
2021-12-15 09:25:28 +0000

    question 1

--- src/main/java/org/example/CargoAirplane.java
+++ src/main/java/org/example/CargoAirplane.java
@@ -1,13 +1,36 @@
 package org.example;
 
-public class CargoAirplane
+public class CargoAirplane extends Airplane
 {
     // fields (ref. Q1)
-    
+    private int maxLoadKilos;
+    private String currentLoad;
 
     // constructor
 
+    public CargoAirplane(String type, int maxLoadKilos, String currentLoad)
+    {
+        super(type);
+        this.maxLoadKilos = maxLoadKilos;
+        this.currentLoad = currentLoad;
+    }
+
+    public int getMaxLoadKilos()
+    {
+        return maxLoadKilos;
+    }
+
+    public String getCurrentLoad()
+    {
+        return currentLoad;
+    }
 
     // toString()
-
+    @Override
+    public String toString() {
+        return "CargoAirplane{" +
+                "maxLoadKilos=" + maxLoadKilos +
+                ", currentLoad='" + currentLoad + '\'' +
+                '}';
+    }
 }
